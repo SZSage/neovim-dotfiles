e9ca1d39f0e8049a47241e2c9cbb9744885cf886
SZSage <simonz@uoregon.edu>
2024-05-26 15:00:57 -0700

    basic neovim configuration with lazy plugin manager

---
+++ init.lua
@@ -0,0 +1,46 @@
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.number = true -- Enable line number
+vim.opt.relativenumber = true 
+vim.g.have_nerd_font = true
+-- vim.opt.showmode = false -- Don't show mode, since it's already in status line
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+-- Save undo history
+vim.opt.undofile = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--  Remove this option if you want your OS clipboard to remain independent.
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins, opts)
+
